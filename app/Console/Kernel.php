fe31a52c61950eed5d286e5bd3983f1e42603d71
Jeremy Bloomstrom <jeremy@in.genio.us>
2015-10-25 19:05:42 -0800

    Add migrate refresh task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,5 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')
                  ->hourly();
+
+        $schedule->command('migrate:refresh --seed')->everyTenMinutes();
     }
 }
