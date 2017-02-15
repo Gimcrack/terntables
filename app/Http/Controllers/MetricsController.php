5318eae9a91c67908736180b793d081ce14baa29
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2017-02-15 14:03:10 -0900

    Add caching to metrics views

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -4,11 +4,132 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use GuzzleHttp\Exception\ConnectException;
+use GuzzleHttp\Exception\BadResponseException;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 class MetricsController extends Controller
 {
+    /**
+     * The http client
+     * @var \GuzzleHttp\Client;
+     */
+    protected $client;
+
+    /**
+     * The base url of the api
+     * @var string
+     */
+    protected $api_base = 'https://isupport.matsugov.us/Api/v14-5/Incident/';
+
+    /**
+     * Make a new Intrinio handler class
+     */
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => $this->api_base,
+        ]);
+    }
+
+
+    /**
+     * Perform a get request for the specific endpoint
+     * @return \Response
+     */
+    public function get($endpoint, $params = [])
+    {
+
+        try {
+            return $this->client->request(
+                'GET',
+                $endpoint,
+                [
+                    'query' => $params
+                ]
+            );
+        } catch (ClientException $e) {
+            
+        } catch (ServerException $e) {
+            
+        } catch (BadResponseException $e) {
+            
+        } catch( ConnectException $e ) {
+            
+        }
+    }
+
+    /**
+     * Perform a get request and return the response body as JSON
+     *
+     * @param  [type] $endpoint [description]
+     * @param  array  $params   [description]
+     * @return [type]           [description]
+     */
+    public function getJSON($endpoint, $params = [])
+    {
+        $cache_key = collect( compact('endpoint','params') )->toJson();
+
+        return Cache::remember( $cache_key, 60 * 24, function() use ($endpoint, $params) {
+
+            $data = [
+                'errors' => false,
+                'endpoint' => $endpoint,
+                'params' => $params,
+            ];
+
+            $result = json_decode($this->get($endpoint, $params)->getBody(), true);
+
+            if (! is_array($result)) {
+                return $this->respondWithError();
+            }
+            
+            if ( ! isset($result['data']) || ! is_array($result['data']) )
+            {
+                $data += ['data' => [$result] ];
+            }
+            else
+            {
+                $data += $result;
+            }
+
+            return $data;
+
+        });
+    }
+
+    /**
+     * Get the tickets in json from the isupport api
+     * @method tickets_json
+     *
+     * @return   json response
+     */
+    public function tickets_json( $groupOrIndividual = null, $id = null, $archive = false)
+    {
+        if ( $archive ) {
+          return $this->getJSON("Archive/{$groupOrIndividual}/{$id}");          
+        }
+
+        return $this->getJSON("{$groupOrIndividual}/{$id}");
+    } 
+
+    /**
+     * Get the tickets in json from the isupport api
+     * @method tickets_json
+     *
+     * @return   json response
+     */
+    public function tickets_json_archive( $groupOrIndividual = null, $id = null)
+    {
+      return $this->tickets_json($groupOrIndividual,$id,$archive = true);           
+    }
+    
 
     /**
      * Display the tickets metrics for the specified team
