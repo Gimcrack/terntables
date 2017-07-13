264fbe31ab4ea41267d290428b6faf83183827c8
Jeremy Bloomstrom <jeremy.bloomstrom@matsugov.us>
2017-07-12 16:41:57 -0800

    Fix assignee filtering

--- resources/views/metrics/tickets.blade.php
+++ resources/views/metrics/tickets.blade.php
@@ -104,7 +104,7 @@
       var barChartData1 = _(response.data)
             // reject for non-person assignees (assignees ending in a number)
             .reject( o => {
-                return !! o.assignee.match(/(.*)\d/gi);
+                return !! o.assignee.match(/(.*)\d/gi) || o.assignee.split(' ').length > 2;
             })
             .groupBy('assignee')
             .map( (o, i) => {
@@ -134,7 +134,7 @@
       var barChartData1 = _(response.data)
             // reject for person assignees (assignees ending in a number)
             .reject( o => {
-                return ! o.assignee.match(/(.*)\d/gi);
+                return ( ! o.assignee.match(/(.*)\d/gi) ) && o.assignee.split(' ').length < 3;
             })
             .groupBy('assignee')
             .map( (o, i) => {
